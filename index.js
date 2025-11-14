fdd4aba4f328424e3df5865d719dcc453348edc7
yuloop255 <yuloop255@gmail.com>
2025-11-14 14:05:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,45 @@
-const express = require("express");
-const app = express();
-const axios = require("axios");
-const os = require('os');
-const fs = require("fs");
-const path = require("path");
-const { promisify } = require('util');
+常量 表达式 = 需求("表达式");
+常量应用 = ();表达式
+常量 axios = 需求("axios");
+常量 操作系统 = 需要('操作系统');
+常量 fs = 需要("fs");
+常量 路径 = 需要("路径");
+常量 { promisify } = 需要('util');
 const exec = promisify(require('child_process').exec);
 const { execSync } = require('child_process');        // 只填写UPLOAD_URL将上传节点,同时填写UPLOAD_URL和PROJECT_URL将上传订阅
-const UPLOAD_URL = process.env.UPLOAD_URL || '';      // 节点或订阅自动上传地址,需填写部署Merge-sub项目后的首页地址,例如：https://merge.xxx.com
-const PROJECT_URL = process.env.PROJECT_URL || '';    // 需要上传订阅或保活时需填写项目分配的url,例如：https://google.com
-const AUTO_ACCESS = process.env.AUTO_ACCESS || false; // false关闭自动保活，true开启,需同时填写PROJECT_URL变量
-const FILE_PATH = process.env.FILE_PATH || './tmp';   // 运行目录,sub节点文件保存目录
-const SUB_PATH = process.env.SUB_PATH || 'sub';       // 订阅路径
+常量 上传地址 = 处理.环境.上传地址 || '      // 节点或订阅自动上传地址,需填写部署Merge-sub项目后的首页地址,例如：https://merge.xxx.com
+常量 PROJECT_URL = 过程.环境.PROJECT_URL || '    // 需要上传订阅或保活时需填写项目分配的url,例如：https://google.com
+常量 AUTO_ACCESS = 过程.环境.AUTO_ACCESS || 假; // 假关闭自动保活，真开启,需同时填写PROJECT_URL变量
+常量 文件路径 = 进程.环境.文件路径 || './tmp';   // 运行目录,子节点文件保存目录
+常量 子路径处理.环境.子路径 || '子';       // 订阅路径
 const PORT = process.env.SERVER_PORT || process.env.PORT || 3000;        // http服务订阅端口
-const UUID = process.env.UUID || '9afd1229-b893-40c1-84dd-51e7ce204913'; // 使用哪吒v1,在不同的平台运行需修改UUID,否则会覆盖
-const NEZHA_SERVER = process.env.NEZHA_SERVER || '';        // 哪吒v1填写形式: nz.abc.com:8008  哪吒v0填写形式：nz.abc.com
-const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
-const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || '';          // 固定隧道域名,留空即启用临时隧道
-const ARGO_AUTH = process.env.ARGO_AUTH || '';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
-const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
-const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
-const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
-const NAME = process.env.NAME || '';                        // 节点名称
+常量 唯一标识符(UUID) = 进程.环境变量.唯一标识符(UUID) || '251e8125-9712-4469-8b2a-b53a0c683f3e'; // 使用哪吒v1,在不同的平台运行需修改UUID,否则会覆盖
+常量 NEZHA_SERVER = 过程.环境.NEZHA_SERVER || '        // 哪吒v1填写形式: nz.abc.com:8008  哪吒v0填写形式：nz.abc.com
+常量 NEZHA_PORT = 过程.环境.NEZHA_PORT || ';;;            // 使用哪吒v1请留空，哪吒v0需填写
+常量 NEZHA_KEY = 过程.环境.NEZHA_KEY || ';;;              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
+常量 ARGO_DOMAIN = 过程.环境.ARGO_DOMAIN || 'cloudcloudcat.vvvvvv.qzz.io';          // 固定隧道域名,留空即启用临时隧道
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiYzZhNzEwYTYzYjlkMTI1YTJjZWQzZmYyZmVhMzU2ODgiLCJ0IjoiZjU2NzYyZTItOTNkYS00MDhlLThmYjktOTliZjI5OGQwMWNlIiwicyI6Ik56Um1OMlZsWkRndE9UTTRPUzAwWkdNMUxUazNPVEF0TXpBM056RXdOVGRoWmpjeiJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
+const ARGO_PORT = process.env.ARGO_PORT || 18008;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
+常量 CFIP = 进程.环境.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
+常量 CFPORT = 进程.环境.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
+常量 名称 = 进程.环境.名称 || '// 节点名称
 
 // 创建运行文件夹
-if (!fs.existsSync(FILE_PATH)) {
+如果(!fs.existsSync(FILE_PATH))那么  
   fs.mkdirSync(FILE_PATH);
-  console.log(`${FILE_PATH} is created`);
-} else {
-  console.log(`${FILE_PATH} already exists`);
+  控制台.日志(`${FILE_PATH} 被创建`);
+} 否则{
+  控制台.日志(`${FILE_PATH} 已存在`);
 }
 
 // 生成随机6位字符文件名
-function generateRandomName() {
-  const characters = 'abcdefghijklmnopqrstuvwxyz';
-  let result = '';
-  for (let i = 0; i < 6; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
+函数 生成随机名称() {
+  常量 字符 = 'abcdefghijklmnopqrstuvwxyz';
+  让 结果 = '';
+  对于 (让 i = 0; i < 6; i++) {
+    结果 += 字符.charAt(Math.floor(Math.random() * 字符.length));
   }
-  return result;
+  返回 结果;
 }
 
 // 全局常量
@@ -53,12 +53,12 @@ let webPath = path.join(FILE_PATH, webName);
 let botPath = path.join(FILE_PATH, botName);
 let subPath = path.join(FILE_PATH, 'sub.txt');
 let listPath = path.join(FILE_PATH, 'list.txt');
-let bootLogPath = path.join(FILE_PATH, 'boot.log');
+让 引导日志路径 = 路径.连接(文件路径, '引导日志');
 let configPath = path.join(FILE_PATH, 'config.json');
 
 // 如果订阅器上存在历史运行节点则先删除
-function deleteNodes() {
-  try {
+函数 删除节点() {
+  尝试{
     if (!UPLOAD_URL) return;
     if (!fs.existsSync(subPath)) return;
 
